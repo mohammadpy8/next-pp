2d5325995c27c4394f823adb372ac47389751f4a
mohammadpy8 <mohammadpy8@gmail.com>
2024-04-30 10:23:05 +0330

    added dot pagination custom

--- components/module/sliderHomePage/SliderHomePage.tsx
+++ components/module/sliderHomePage/SliderHomePage.tsx
@@ -7,12 +7,16 @@ import { ShapeSvg } from "@/public/svg";
 
 const SliderHomePage: FC = () => {
   var settings = {
-    // dots: true,
+    dots: true,
     rtl: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    customPaging: function () {
+      return <div className="dot"></div>;
+    },
+    dotsClass: "slick-dots slick-thumb"
   };
 
   return (
--- components/module/sliderHomePage/style.ts
+++ components/module/sliderHomePage/style.ts
@@ -28,6 +28,31 @@ const SliderCustom = styled(Slider)(() => ({
   "& .slick-slide div div": {
     outline: "none",
   },
+  "& .slick-dots.slick-thumb": {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  "& .slick-dots.slick-thumb li": {
+    width: "6px",
+    height: "6px",
+    margin: "0 3px",
+    borderRadius: "999px",
+    transition: "all 0.5s ease-in-out",
+    willChange: "width",
+    backgroundColor: "black",
+  },
+  "& .slick-dots.slick-thumb li.slick-active": {
+    backgroundColor: "#417F56",
+    width: "24px",
+  },
+  "& .slick-dots": {
+    display: "flex !important",
+    position: "absolute",
+    bottom: "15px",
+    right: "217px",
+    zIndex: 10,
+  },
 }));
 
 export default SliderCustom;
