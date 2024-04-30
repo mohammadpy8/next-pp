c4adca2877294de2e7ecaf13df8cdc5d640ff28b
mohammadpy8 <mohammadpy8@gmail.com>
2024-04-30 14:11:11 +0330

    menu cc

--- components/module/menuHomePage/MenuHomePage.tsx
+++ components/module/menuHomePage/MenuHomePage.tsx
@@ -8,42 +8,23 @@ const MenuHomePage: FC = () => {
       <div className="bg-primary-bgColor w-48 h-14 rounded-l-2xl flex justify-center items-center">
         <span className="font-bold text-[20px]">منوی رستوران</span>
       </div>
-      <div className="mt-4 mx-5 flex justify-between gap-x-40 overflow-x-scroll h-[200px] menu-box">
-        <div className=" relative">
-          <div>
+      <div className="mt-4 mx-5 flex justify-between gap-x-40 overflow-x-scroll h-[250px] menu-box">
+        <div className="bg-red-500 w-[150px]">
+          <div className="flex justify-center -pt-12">
             <Image
-              src="/image/11.jpg"
+              src="/image/1.png"
+              width={150}
+              height={150}
               alt="1"
-              width={120}
-              height={100}
-              className="w-[120px] h-[120px] rounded-full mr-[10px] z-10 absolute"
+              className="w-[120px] h-[120px] rounded-full"
             />
           </div>
-          <div className="bg-primary-base w-[140px] h-[85px] rounded-lg shadow-xl absolute top-[75px] z-0">
-            <ButtonComponent
-              textButton="غذای اصلی"
-              customStyles="bg-white shadow-xl font-bold text-[15px] absolute -bottom-2 right-[25px] text-gray-600"
-            />
+          <div className="flex justify-center">
+            <div className="bg-primary-base w-[140px] h-[85px] rounded-lg shadow-xl"></div>
           </div>
+          <div></div>
         </div>
-        <div className="relative">
-          <div>
-            <Image
-              src="/image/11.jpg"
-              alt="1"
-              width={120}
-              height={100}
-              className="w-[120px] h-[120px] rounded-full mr-[10px] z-10 absolute"
-            />
-          </div>
-          <div className="bg-primary-base w-[140px] h-[85px] rounded-lg shadow-xl absolute top-[75px] z-0">
-            <ButtonComponent
-              textButton="غذای اصلی"
-              customStyles="bg-white shadow-xl font-bold text-[15px] absolute -bottom-2 right-[25px] text-gray-600"
-            />
-          </div>
-        </div>
-        <div className=" relative">
+        {/* <div className="relative">
           <div>
             <Image
               src="/image/11.jpg"
@@ -94,6 +75,23 @@ const MenuHomePage: FC = () => {
             />
           </div>
         </div>
+        <div className=" relative">
+          <div>
+            <Image
+              src="/image/11.jpg"
+              alt="1"
+              width={120}
+              height={100}
+              className="w-[120px] h-[120px] rounded-full mr-[10px] z-10 absolute"
+            />
+          </div>
+          <div className="bg-primary-base w-[140px] h-[85px] rounded-lg shadow-xl absolute top-[75px] z-0">
+            <ButtonComponent
+              textButton="غذای اصلی"
+              customStyles="bg-white shadow-xl font-bold text-[15px] absolute -bottom-2 right-[25px] text-gray-600"
+            />
+          </div>
+        </div> */}
       </div>
     </div>
   );
